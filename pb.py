6a9b398f9d15955860b899d405f8171402588a2e
jworl <jworl@users.noreply.github.com>
2019-07-15 18:12:22 -0400

    Update pb.py

--- pb.py
+++ pb.py
@@ -3,39 +3,61 @@
 
 from sys import argv
 from getpass import getpass
-# import xml.etree.ElementTree as ET
+
 try:
     import xmltodict
 except ImportError as e:
     print e
+
 import requests
+import json
+import hashlib
 
-if len(argv) != 3:
-    print "Usage: {} $firewall $username".format(argv[0])
-    exit(2)
+def _REQ(URL,FILENAME):
+    R = requests.get(URL, verify=False, timeout=5)
+    if R.status_code is requests.codes.ok:
+        with open(FILENAME, 'wb') as f:
+            f.write(R.content)
+        with open(FILENAME) as x:
+            dict = xmltodict.parse(x.read())
+        return dict
+    elif R.status_code == 403:
+        print "Invalid credentials"
+        exit(2)
+    else:
+        print "Wonk wonk wonnnnnkkkkk"
+        print R.raise_for_status()
+        exit(2)
 
-FW = argv[1]
-USER = argv[2]
-P = getpass(prompt='Enter your passphrase: ')
-dev = "https://{}/api/?".format(FW)
-url = "{}?type=keygen&user={}&password={}".format(dev, USER, P)
+def _GET_TOKEN(D, U, p):
+    TOKEN_URL = "{}type=keygen&user={}&password={}".format(D, U, p)
+    TOKEN = _REQ(TOKEN_URL, 'token.xml')
 
-try:
-    resp = requests.get(url, verify=False)
-    with open('token.xml', 'wb') as f:
-        f.write(resp.content)
-except IOError as e:
-    print e.error_code
+    if TOKEN['response']['@status'] == "success":
+        return str(TOKEN['response']['result']['key'])
+    else:
+        print "something went wrong"
+        print TOKEN['response']
+        exit(2)
 
-with open('token.xml') as x:
-    tree = xmltodict.parse(x.read())
+def _GET_BACKUP(F, D, T):
+    PULL_CONFIG = "{}type=export&category=configuration&key={}".format(D, T)
+    CONFIG = _REQ(PULL_CONFIG, '{}_backup.xml'.format(F))
+    HASH = hashlib.md5(json.dumps(CONFIG))
+    return HASH.hexdigest()
 
-if tree['response']['@status'] == "success":
-    token = str(tree['response']['result']['key'])
-else:
-    print "something went wrong"
-    print tree['response']
+def main():
+    FW = argv[1]
+    USER = argv[2]
+    P = getpass(prompt='Enter your passphrase: ')
+    dev = "https://{}/api/?".format(FW)
+    token = _GET_TOKEN(dev, USER, P)
+    conf_fp = _GET_BACKUP(FW, dev, token)
+    print conf_fp
 
-url = "{}?type=export&category=configuration&key={}".format(dev, token)
-resp = requests.get(url, verify=False)
-print resp.content
+if __name__ == "__main__":
+    if len(argv) != 3:
+        print "Usage: {} $firewall $username".format(argv[0])
+        exit(2)
+    else:
+        main()
